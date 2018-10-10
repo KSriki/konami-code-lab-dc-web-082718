9624e98aae89d2c38f90a797373b08b7f2206f42
github username <github email address>
2018-10-10 17:19:15 -0400

    Done.

--- konami_code.js
+++ konami_code.js
@@ -13,4 +13,30 @@ const codes = [
 
 function init() {
   // your code here
+  addKonamiListener();
+}
+
+
+
+function addKonamiListener(){
+    document.body.addEventListener("keydown",konami);
+}
+
+let index = 0;
+function konami(event){
+
+    let key = event.key;
+
+    if(key === codes[index]){
+        index++;
+        if(index === codes.length){
+            window.alert("YOU GET 100 EXTRA LIVES");
+            console.log("congrats");
+            index = 0;
+        }
+    }
+    else{
+        index = 0;
+    }
+
 }
